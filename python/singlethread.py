2d165d86bca20e8ec00b9f0b2872abde0cf15cda
akswart <akswart@gmail.com>
2023-03-07 22:23:21 -0700

    Increased is_prime speed by a factor of 2. Added a few other data population options, doesn't make a huge difference in speed.

--- python/singlethread.py
+++ python/singlethread.py
@@ -15,7 +15,9 @@ def is_prime(test_num):
     # Tests if k is prime
     if test_num == 2:
         return True
-    for test_div in range(2,math.ceil(np.sqrt(test_num))+1):
+    if test_num % 2 == 0:
+        return False
+    for test_div in range(3,math.ceil(np.sqrt(test_num))+1,2):
         if test_num % test_div == 0:
             return False
     return True
@@ -25,23 +27,54 @@ def prime_list(my_list):
 
 def non_parallel_primes(end_power):
     nump = 0
-    for i in np.arange(1,end_power,dtype=np.int64):
+    for i in np.arange(1,end_power+1,dtype=np.int64):
         ap = 10**(i-1)
         bp = 10**i
         p = prime_list(range(ap,bp))
         nump += len(p)
-    return nump 
+    return nump
+ 
+def test1(end_power):
+    nump = len(prime_list(range(1,int(10**end_power))))
+    return nump
+
+def test2(end_power):
+    nump = 0
+    for i in range(1,int(10**end_power)):
+        if is_prime(i):
+            nump += 1
+    return nump
+
+def benchmark1(end_power,num_iter):
+    t = []
+    for i in range(num_iter):
+        t0 = time.time()
+        n = test1(end_power)
+        t1 = time.time()
+        t.append(t1-t0)
+    #print("New:",n)
+    return sum(t)/len(t)
+
+def benchmark2(end_power,num_iter):
+    t = []
+    for i in range(num_iter):
+        t0 = time.time()
+        n = test2(end_power)
+        t1 = time.time()
+        t.append(t1-t0)
+    #print("New:",n)
+    return sum(t)/len(t)
 
 def singleprime(end_power,num_iter,debug = False):
     t = []
     for i in range(num_iter):
         t0 = time.time()
-        non_parallel_primes(end_power)
+        n = non_parallel_primes(end_power)
         t1 = time.time()
         t.append(t1-t0)
         if debug:
             print("Done: ",t1-t0)
-        
+    #print("Old:",n)
     t_avg = sum(t)/len(t)
     outlist = ["Python",str(end_power),str(num_iter),str(t_avg)]
     
@@ -60,9 +93,23 @@ def singleprime(end_power,num_iter,debug = False):
     return t_avg
 
 if __name__ == "__main__":
-    power_step = .25
-    end_power = 6
+    num_iter = 10
+    score = 0
+    for p in np.arange(5,8,.25):
+        t_avg1 = benchmark1(p, num_iter)
+        t_avg2 = benchmark2(p, num_iter)
+        
+        print("p = {}: Method1 {:5f}, Method2 {:5f}".format(p,t_avg1,t_avg2))
+        #t_avgold = singleprime(p, num_iter)
+        #print("p = {:d}: Method1 {:5f}, Old Method {:5f}".format(p,t_avg1,t_avgold))
+        #if t_avg1 <= t_avgold:
+        #    score += 1
+              
+    """
+    power_step = .1
+    end_power = 7
     for p in np.arange(1,end_power+power_step,power_step):
         t_avg = singleprime(p, 5)
+        print("Running with pow:{}, time: {}".format(p,t_avg))
     # Get a baseline for single threaded performance
-    
+    """
